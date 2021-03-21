2f1c37b18e4548afc8ee8549d61cc219c2290d37
Oladayo Fagbemi <oofagbemi@outlook.com>
2021-03-21 02:07:30 +0100

    refactored pdfclient

--- src/lib/PdfClient.ts
+++ src/lib/PdfClient.ts
@@ -2,20 +2,16 @@ import { PDFDocument } from "pdf-lib";
 import * as fs from "fs";
 import { flatten } from "lodash";
 import { File } from "../types";
+const fsPromise = fs.promises;
 export async function mergePdf(files: Array<File>): Promise<Uint8Array> {
   const pdfDoc = await PDFDocument.create();
-  const fsPromise = fs.promises;
+
   let resultPdfBytes;
   try {
-    //readfiles by filepath
-    const fileBufferPromises = files.map((file) =>
-      fsPromise.readFile(file.path)
-    );
-    const donorPdfBytes = await Promise.all(fileBufferPromises);
+    //readfileBytes from filepath
+    const donorPdfBytes = await readBytes(files);
     //load them into a PDF document
-    const donorPdfDocs = await Promise.all(
-      donorPdfBytes.map((donorPdfByte) => PDFDocument.load(donorPdfByte))
-    );
+    const donorPdfDocs = await loadToPdf(donorPdfBytes);
     //copy pages into the final pdf documents
     const pagesToBeAdded = await Promise.all(
       donorPdfDocs.map((doc) => pdfDoc.copyPages(doc, doc.getPageIndices()))
@@ -29,4 +25,14 @@ export async function mergePdf(files: Array<File>): Promise<Uint8Array> {
     console.log(error);
   }
   return resultPdfBytes;
+}
+async function readBytes(filePaths: File[]): Promise<Buffer[]> {
+  return await Promise.all(
+    filePaths.map((file) => fsPromise.readFile(file.path))
+  );
+}
+async function loadToPdf(bufferArray: Buffer[]): Promise<PDFDocument[]> {
+  return await Promise.all(
+    bufferArray.map((donorPdfByte) => PDFDocument.load(donorPdfByte))
+  );
 }
